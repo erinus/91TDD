bc4414dabd44419010a2dfd1ae32cf4b29665511
erinus <erinus.startup@gmail.com>
2017-06-14 22:50:52 +0800

    Add production code

--- HomeworkDay1/DataSplitter/TableSplitter.cs
+++ HomeworkDay1/DataSplitter/TableSplitter.cs
@@ -8,30 +8,35 @@ namespace DataSplitter
 {
 	public class TableSplitter
 	{
-		public IEnumerable<int> SplitAndGetSum<TSource>(IEnumerable<TSource> source, int pageSize,
+		public IEnumerable<int> SplitAndGetSum<TSource>(IEnumerable<TSource> sources, int pageSize,
 			Func<TSource, int> selector)
 		{
 			List<int> result = new List<int>();
 
+			// 每次取出訂單筆數為 0 或負數時，應觸發 ArgumentException
 			if (pageSize == 0 || pageSize < 0)
 			{
 				throw new ArgumentException();
 			}
 
-			if (selector.Method.Name.Equals("GetCostCell"))
+			// 將資料集依序取出單筆資料後，依指定欄位取出值放入 List
+			IEnumerator<TSource> enumarator = sources.GetEnumerator();
+
+			List<int> extracted = new List<int>();
+
+			while (enumarator.MoveNext())
 			{
-				result = new List<int>
-				{
-					6, 15, 24, 21
-				};
+				TSource source = enumarator.Current;
+
+				extracted.Add(selector(source));
 			}
 
-			if (selector.Method.Name.Equals("GetRevenueCell"))
+			// 按照 pageSize 每次取出對應數量的值並加總後放入回傳資料集
+			for (int i = 0; i < extracted.Count; i += pageSize)
 			{
-				result = new List<int>
-				{
-					50, 66, 60
-				};
+				int sum = extracted.GetRange(i, i + pageSize < extracted.Count ? pageSize : extracted.Count - i).Sum();
+
+				result.Add(sum);
 			}
 
 			return result;
