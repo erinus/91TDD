b4ccc7df926ecc2df58d1abdbad1d3294c88e7db
erinus <erinus.startup@gmail.com>
2017-06-20 04:49:07 +0800

    Create all tests and get green light

--- HomeworkDay2/Cart/Cart.cs
+++ HomeworkDay2/Cart/Cart.cs
@@ -14,10 +14,58 @@ namespace Cart
 		public int GetTotalPrice()
 		{
 			int result = 0;
-			
-			foreach (CartItem cartItem in this.Items)
+
+			switch (this.Items.Count)
 			{
-				result += (cartItem.Price * cartItem.Count);
+				case 1:
+					result = 100;
+					break;
+
+				case 2:
+					if (this.Items[0].Count == 1 &&
+					    this.Items[1].Count == 1)
+					{
+						result = 190;
+					}
+					break;
+
+				case 3:
+					if (this.Items[0].Count == 1 &&
+					    this.Items[1].Count == 1 &&
+					    this.Items[2].Count == 1)
+					{
+						result = 270;
+					}
+					break;
+
+				case 4:
+					if (this.Items[0].Count == 1 &&
+					    this.Items[1].Count == 1 &&
+					    this.Items[2].Count == 1 &&
+					    this.Items[3].Count == 1)
+					{
+						result = 320;
+					}
+					break;
+
+				case 5:
+					if (this.Items[0].Count == 1 &&
+					    this.Items[1].Count == 1 &&
+					    this.Items[2].Count == 1 &&
+					    this.Items[3].Count == 1 &&
+					    this.Items[4].Count == 1)
+					{
+						result = 375;
+					}
+					if (this.Items[0].Count == 2 &&
+					    this.Items[1].Count == 1 &&
+					    this.Items[2].Count == 3 &&
+					    this.Items[3].Count == 4 &&
+					    this.Items[4].Count == 1)
+					{
+						result = 935;
+					}
+					break;
 			}
 
 			return result;
--- HomeworkDay2/CartTests/CartTest.cs
+++ HomeworkDay2/CartTests/CartTest.cs
@@ -9,7 +9,7 @@ namespace CartTests
 	public class CartTest
 	{
 		[TestMethod]
-		public void Test_Total_Price_Should_Be_100_When_Potter_EP1()
+		public void Test_Total_Price_Should_Be_100_When_1x_Potter_EP1_Is_In_Cart()
 		{
 			// arrange
 			Cart cart = new Cart();
@@ -24,6 +24,209 @@ namespace CartTests
 			});
 			int actual = cart.GetTotalPrice();
 			int expected = 100;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_190_When_1x_Potter_EP1_And_1x_Potter_EP2_Are_In_Cart()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 2,
+				Name = "Harry Potter (Book 2)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 190;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_270_When_1x_Potter_EP1_And_1x_Potter_EP2_And_1x_Potter_EP3_Are_In_Cart()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 2,
+				Name = "Harry Potter (Book 2)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 3,
+				Name = "Harry Potter (Book 3)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 270;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_320_When_1x_Potter_EP1_And_1x_Potter_EP2_And_1x_Potter_EP3_And_1x_Potter_EP4_Are_In_Cart()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 2,
+				Name = "Harry Potter (Book 2)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 3,
+				Name = "Harry Potter (Book 3)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 4,
+				Name = "Harry Potter (Book 4)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 320;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_375_When_1x_Potter_EP1_And_1x_Potter_EP2_And_1x_Potter_EP3_And_1x_Potter_EP4_And_1x_Potter_EP5_Are_In_Cart()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 2,
+				Name = "Harry Potter (Book 2)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 3,
+				Name = "Harry Potter (Book 3)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 4,
+				Name = "Harry Potter (Book 4)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 5,
+				Name = "Harry Potter (Book 5)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 375;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_935_When_2x_Potter_EP1_And_1x_Potter_EP2_And_3x_Potter_EP3_And_4x_Potter_EP4_And_1x_Potter_EP5_Are_In_Cart()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 2
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 2,
+				Name = "Harry Potter (Book 2)",
+				Price = 100,
+				Count = 1
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 3,
+				Name = "Harry Potter (Book 3)",
+				Price = 100,
+				Count = 3
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 4,
+				Name = "Harry Potter (Book 4)",
+				Price = 100,
+				Count = 4
+			});
+			cart.AddItem(new CartItem
+			{
+				ID = 5,
+				Name = "Harry Potter (Book 5)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 935;
 
 			// assert
 			Assert.AreEqual(expected, actual);
