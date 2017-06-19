94a1f31e31dc4539bbf560a75ff45dd410fd753a
erinus <erinus.startup@gmail.com>
2017-06-20 04:06:56 +0800

    First, create UnitTest Project

---
+++ HomeworkDay2/CartTests/CartTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartTests
+{
+	[TestClass]
+	public class CartTest
+	{
+		[TestMethod]
+		public void Test_Total_Price_Should_Be_100_When_Potter_EP1()
+		{
+			// arrange
+			Cart cart = new Cart();
+
+			// act
+			cart.AddItem(new CartItem
+			{
+				ID = 1,
+				Name = "Harry Potter (Book 1)",
+				Price = 100,
+				Count = 1
+			});
+			int actual = cart.GetTotalPrice();
+			int expected = 100;
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ HomeworkDay2/CartTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CartTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CartTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("512b03f3-61ca-49c8-b21f-7eaf0f285c92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
